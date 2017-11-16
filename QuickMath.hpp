074e28beb11b93e6e93044b6b0173ec9017cd46f
ChernovAndrey <chernov.andrey.98@mail.ru>
2017-11-16 12:08:27 +0300

    add second newton

--- QuickMath.hpp
+++ QuickMath.hpp
@@ -21,7 +21,7 @@ public:
         float y = *(float *) &i;
         if(flagApprox) {
             float q = 1.0/p;
-            return (1 - p) * y + p*(x/quickAppr(y,q-1,false));
+            return (1 - p) * y + p*(x/quickAppr(y, static_cast<float>(q - 1.0), false));
         }
         return y;
     }
@@ -41,29 +41,53 @@ public:
         return res;
     }
 
+    static double powNatural(double x, int k) // возведение x в степень k
+    {
+        float res = 1.0;
+        while (k)
+        {
+            if (k & 1)
+                res *= x;
+            x *= x;
+            k >>= 1;
+        }
+        return res;
+    }
+
+
     static float powInverse(float x, int q) { //equal pow(x,1/q)
         int i = *(int *) &x;
         i = static_cast<int>(0x3f7a3bea + (1.0/q) * (i - 0x3f7a3bea));
         float y = *(float *) &i;
-        // return y;
 
-        //for p =1/3
         y = (1-1.0/q)*y+(1.0/q)*x/powNatural(y,q-1);
         return (1-1.0/q)*y+(1.0/q)*x/powNatural(y,q-1);
     }
 
-
-    static float quickOneThird(float x, double p) {
-        // float xhalf = 0.5f * v[j];
-
+    static float powInverse2(float x, int q) { //equal pow(x,1/q)
         int i = *(int *) &x;
-        i = static_cast<int>(0x3f7a3bea + p * (i - 0x3f7a3bea));
+        i = static_cast<int>(0x3f7a3bea + (1.0/q) * (i - 0x3f7a3bea));//не очень как то
         float y = *(float *) &i;
-        // return y;
 
-        //for p =1/3
-        return 0.6666667*y+0.3333333*x/(y*y);
+      //  y = (1-1.0/q)*y+(1.0/q)*x/powNatural(y,q-1);
+        float a = powNatural(y,q-2);
+        float b = a*y;//powNatural(y,q-1)
+        float c = b*y;//powNatural(y,q)
+        float cx= c - x;
+        float d = ((q-1)*a*cx)/(b*b*q);
+        return y-(cx)/(q*b)*(1+0.5*d);
     }
+
+
+    static double powInverse(double x, int q) { //equal pow(x,1/q)
+        long i = *(long *) &x;
+        i = static_cast<long>(0x3FEF47724901B800 + (1.0 / q) * (i - 0x3FEF47724901B800));
+        double y = *(double*) &i;
+
+        y = (1-1.0/q)*y+(1.0/q)*x/powNatural(y,q-1);
+        return (1-1.0/q)*y+(1.0/q)*x/powNatural(y,q-1);
+    }
+
 
 
 /*from wikipedia Quake 3*/
@@ -84,6 +108,30 @@ public:
     }
 
     static float quickPow(float x, int n, int m){//equial pow(x,n/m)
+        // return powNatural(powInverse(x,m),n);
+  /*      clock_t start = clock();
+        float a = powNatural(x,n);
+        clock_t end = clock();
+        float seconds = (float) (end - start) / CLOCKS_PER_SEC;
+
+      clock_t start2 = clock();
+        float res = powNatural(x,n);
+        clock_t end2 = clock();
+        float seconds2 = (float) (end2 - start2) / CLOCKS_PER_SEC;
+         std::cout<<"timeInv:"<<seconds<<std::endl;
+         std::cout<<"timeNat:"<<seconds2<<std::endl;
+        return res;*/
+
+        return powInverse(powNatural(x,n),m);
+    }
+
+    static float quickPow2(float x, int n, int m){//equial pow(x,n/m)
+      //   return powNatural(powInverse2(x,m),n);
+        return powInverse2(powNatural(x,n),m);
+    }
+
+
+    static double quickPow(double x, int n, int m){//equial pow(x,n/m)
         // return powNatural(powInverse(x,m),n);
         return powInverse(powNatural(x,n),m);
     }
--- main.cpp
+++ main.cpp
@@ -8,38 +8,63 @@
 #include "QuickMath.hpp"
 
 using namespace std;
+using number = float;
 int n = 10000;
 
 
 
 
-vector<float> createRandomVector() {
+vector<number> createRandomVector() {
     srand(static_cast<unsigned int>(time(0)));
-    vector<float> v;
-    float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-    for (int i = 0; i < n; i++) v.push_back(static_cast <float> (rand()) / static_cast <float> (10000000));
+    vector<number> v;
+    number r = static_cast <number> (rand()) / static_cast <number> (RAND_MAX);
+    for (int i = 0; i < n; i++) v.push_back(static_cast <number> (rand()) / static_cast <number> (10000000));
     return v;
 }
 
 
 
+void printDiff(const vector<number> &v1, const vector<number> &v2, int &nMax,number &max){
+    for(int i=0;i<v1.size();i++){
+        number diff=abs(v1[i]-v2[i]);
+        if(max<diff) {
+            nMax=i;
+            max=diff;
+        }
+        cout<<diff<<" ";
+    }
+
+}
+
+//for number K=3FEF47724901B800
 int main() {
-    vector<float> v = createRandomVector();
+    vector<number> v = createRandomVector();
 
-    vector<float> vApprox(v.size());
-    vector<float> vAccur(v.size());
+    vector<number> vApprox(v.size());
+    vector<number> vApprox2(v.size());
+    vector<number> vAccur(v.size());
 
+    //number p = 1.0/3;
 
-  //  float p = 1.0/3;
-
-    int n=5;
+    int n=2;
     int m =3;
     clock_t startQ = clock();
     for (int i = 0; i < v.size(); i++) {
         vApprox[i]=QuickMath::quickPow(v[i],n,m);
+    //    vApprox[i]=QuickMath::quickAppr(v[i],(1.0*n)/m,true);
     }
     clock_t endQ = clock();
-    double secondsQ = (double) (endQ - startQ) / CLOCKS_PER_SEC;
+    number secondsQ = (number) (endQ - startQ) / CLOCKS_PER_SEC;
+
+
+    clock_t startQ2 = clock();
+    for (int i = 0; i < v.size(); i++) {
+        vApprox2[i]=QuickMath::quickPow2(v[i],n,m);
+        //    vApprox[i]=QuickMath::quickAppr(v[i],(1.0*n)/m,true);
+    }
+    clock_t endQ2 = clock();
+    number secondsQ2 = (number) (endQ2 - startQ2) / CLOCKS_PER_SEC;
+
 
 
     clock_t startS = clock();
@@ -48,8 +73,9 @@ int main() {
     }
 
     clock_t endS = clock();
-    double secondsS = (double) (endS - startS) / CLOCKS_PER_SEC;
+    number secondsS = (number) (endS - startS) / CLOCKS_PER_SEC;
     cout << "quick:" << secondsQ << endl;
+    cout<<"quick2:"<<secondsQ2<<endl;
     cout << "standard:" << secondsS << endl;
 
     cout<<endl<<endl;
@@ -65,17 +91,25 @@ int main() {
     }
     cout << endl;
 
-    float max =0;
-    int nMax=0;
-    for(int i=0;i<vAccur.size();i++){
-        float diff=abs(vAccur[i]-vApprox[i]);
-        if(max<diff) {
-            nMax=i;
-            max=diff;
-        }
-        cout<<diff<<" ";
+    for(int i=0;i<vApprox2.size();i++){
+        cout<<vApprox2[i]<<" ";
     }
     cout << endl;
+
+    number max =0;
+    number max2 =0;
+    int nMax=0;
+    int nMax2=0;
+
+    cout<<"1:"<<endl;
+    printDiff(vAccur,vApprox,nMax,max);
+    cout<<endl;
+    cout<<"2:"<<endl;
+    printDiff(vAccur,vApprox2,nMax2,max2);
+    cout << endl;
     cout<<"maxDiff="<<max<<endl;
+    cout<<"maxDiff2="<<max2<<endl;
     cout<<"killer elements:"<<vAccur[nMax]<<" "<<vApprox[nMax]<<endl;
+    cout<<"killer elements2:"<<vAccur[nMax2]<<" "<<vApprox[nMax2]<<endl;
+    return 0;
 }
